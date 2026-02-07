6eecdaf793f6f8ee3d2f287b9b8f39705304b170
tm-barry <travis.m.barry@gmail.com>
2026-02-06 18:43:03 -0600

    update version 0.8.4

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
     QQuickStyle::setStyle("Fusion");
     app.setOrganizationName("tm-barry");
     app.setApplicationName("BarryAppLauncher");
-    app.setApplicationVersion("0.8.3");
+    app.setApplicationVersion("0.8.4");
     app.setWindowIcon(QIcon(":/assets/icons/barryapplauncher.svg"));
 
     QString fileArg;
